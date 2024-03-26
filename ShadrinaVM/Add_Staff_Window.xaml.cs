ed74378ae35a29fb97c12130715e165e7705c486
SHadrina.V.M <SHadrina.V.M@PC404-05>
2024-03-26 11:16:43 +0300

    Add project files.

---
+++ ShadrinaVM/Add_Staff_Window.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ShadrinaVM
+{
+    /// <summary>
+    /// Interaction logic for AddStaff.xaml
+    /// </summary>
+    public partial class AddStaff : Window
+    {
+        public AddStaff()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ShadrinaVM/Admin_Staff_Window.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ShadrinaVM
+{
+    /// <summary>
+    /// Interaction logic for AdminStaff.xaml
+    /// </summary>
+    public partial class AdminStaff : Window
+    {
+        public AdminStaff()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
